f960673a0651f7435bedc87255043742bc8dc57f
Jacky <kuoyichun1102@gmail.com>
2016-05-06 18:09:05 +0800

    log in using form

--- src/client/components/chat/form.js
+++ src/client/components/chat/form.js
@@ -1,9 +1,56 @@
 import {ElementComponent} from "../../lib/component";
 import $ from "jquery";
 
+import {Observable} from "rxjs";
+
 export class ChatFormComponent extends ElementComponent{
-    constructor() {
+    constructor(usersStore) {
         super("div");
+        this._users = usersStore;
         this.$element.addClass("chat-form");
+    }
+
+    _onAttach() {
+        this._$error = $(`<div class = "chat-error" />`).appendTo(this.$element);
+        this._$input = $(`<input type="text" class="chat-input"/>`).appendTo(this.$element);
+        this._users.currentUser$.comSubscribe(this, user => {
+            this._$input.attr("placeholder", user.isLoggedIn ? "" : "Enter a username to log in");
+        });
+
+        Observable.fromEvent(this._$input, "keydown")
+            //Get value
+            .filter(e => e.keyCode === 13)
+            .do(e => e.preventDefault())
+            .map(e => e.target.value.trim())
+            .filter(e => e.length)
+
+            //Login or send message
+            .withLatestFrom(this._users.currentUser$)
+            .flatMap(([value, user]) => {
+                return user.isLoggedIn ? this._sendMessage$(value) : this._login$(value);
+            })
+
+            //Display mesaage
+            .comSubscribe(this, response => {
+                if (response && response.error)
+                    this._$error.show().text(response.error.message);
+                else
+                    this._$error.hide();
+            });
+
+    }
+
+    _sendMessage$(value) {
+        return Observable.empty();
+    }
+
+    _login$(username) {
+        this._$input.attr("disabled", "disabled");
+        return this._users.login$(username).catchWrap()
+            .do(() => this._$input.val(""))
+            .finally(() => {
+                this._$input.attr("disabled", null);
+                this._$input.focus();
+            });
     }
 }
--- src/shared/operators.js
+++ src/shared/operators.js
@@ -15,3 +15,7 @@ Observable.prototype.safeSubscribe = function(next, error, complete) {
     );
     return subscription;
 };
+
+Observable.prototype.catchWrap = function() {
+    return this.catch(error => Observable.of({error: error}));
+};
